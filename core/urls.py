1883dee36041ee9750147366b6d9145078e6219e
Niharika <niharikasaini06@gmail.com>
2020-11-21 15:17:23 +0530

    added functionalities

---
+++ core/urls.py
@@ -0,0 +1,41 @@
+from django.urls import path
+from django.conf.urls import include
+from .views import (
+    ItemDetailView,
+    CheckoutView,
+    HomeView,
+    OrderSummaryView,
+    add_to_cart,
+    remove_from_cart,
+    remove_single_item_from_cart,
+    PaymentView,
+    AddCouponView,
+    RequestRefundView,
+    search,
+    categ1,
+    categ2,
+    categ3
+
+)
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
+    path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
+    path('product/<slug>/', ItemDetailView.as_view(), name='product'),
+    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
+    path('add-coupon/', AddCouponView.as_view(), name='add-coupon'),
+    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
+    path('remove-item-from-cart/<slug>/', remove_single_item_from_cart, name='remove-single-item-from-cart'),
+    path('payment/<payment_option>/', PaymentView.as_view(), name='payment'),
+    path('request-refund/', RequestRefundView.as_view(), name='request-refund'),
+    path('search/', search, name='search'),
+    path('categ1/', categ1, name='categ1'),
+    path('categ2/', categ2, name='categ2'),
+    path('categ3/', categ3, name='categ3')
+
+
+
+]
